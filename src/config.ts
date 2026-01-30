d9f35592f216505468ed187608a82814d503f271
work-wangxvvv <wangxvvv0213@163.com>
2026-01-30 19:34:18 +0800

    配置

--- src/config.ts
+++ src/config.ts
@@ -23,9 +23,9 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "浮光跃金",
-	subtitle: "One demo website",
+	subtitle: "我的成长之路",
 	siteURL: "http://wangxvvv.top/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2023-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
@@ -215,7 +215,7 @@ export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 	},
 	zIndex: -1, // 层级，确保壁纸在背景层
 	opacity: 1, // 壁纸透明度
-	blur: 1, // 背景模糊程度
+	blur: 0.3, // 背景模糊程度
 };
 
 export const navBarConfig: NavBarConfig = {
