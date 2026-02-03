76df098b41a573f8310f0adb6d973a5795d9be62
work-wangxvvv <wangxvvv0213@163.com>
2026-02-03 12:21:59 +0800

    字体优化

--- src/config.ts
+++ src/config.ts
@@ -195,7 +195,7 @@ export const siteConfig: SiteConfig = {
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "霞鹜文楷",
+			fontFamily: "LXGWWenKai-Regular",
 			fontWeight: "500",
 			localFonts: ["LXGWWenKai-Regular.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
